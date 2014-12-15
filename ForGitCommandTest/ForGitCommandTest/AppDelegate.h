e508a9d12259592563e307d560e0f7e7f8d89d56
guofeng <guofeng@belo-inc.com>
2014-12-15 16:46:13 +0800

    addfile

---
+++ ForGitCommandTest/ForGitCommandTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ForGitCommandTest
+//
+//  Created by Bill on 14-12-15.
+//  Copyright (c) 2014年 CNHaiTao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
